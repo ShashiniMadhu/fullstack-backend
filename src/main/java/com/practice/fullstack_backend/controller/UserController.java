9bcba2b9870e1842510b78e65d6380a9e4db6310
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-05-24 19:51:31 +0530

    Add an annotation to connect with the frontend

--- src/main/java/com/practice/fullstack_backend/controller/UserController.java
+++ src/main/java/com/practice/fullstack_backend/controller/UserController.java
@@ -4,14 +4,12 @@ import com.practice.fullstack_backend.model.User;
 import com.practice.fullstack_backend.repository.UserRepository;
 import jakarta.persistence.GeneratedValue;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000") //Connect with frontend
 public class UserController {
 
     @Autowired
