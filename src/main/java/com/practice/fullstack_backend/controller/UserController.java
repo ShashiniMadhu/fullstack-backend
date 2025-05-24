a24e8a1e5e83c8b39410e24f01b9f938e2696100
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-05-24 18:07:36 +0530

    Initialize fullstack-backend

---
+++ src/main/java/com/practice/fullstack_backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.practice.fullstack_backend.controller;
+
+import com.practice.fullstack_backend.model.User;
+import com.practice.fullstack_backend.repository.UserRepository;
+import jakarta.persistence.GeneratedValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/user")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/practice/fullstack_backend/model/User.java
@@ -0,0 +1,49 @@
+package com.practice.fullstack_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
